15856532c7456900bb3e1ab44ec5582851327b16
UxTom <thomas.h.brown@icloud.com>
2025-12-09 15:03:11 +0000

    Created disclosure-statuses.json Called as disclosureStatuses in session-data-defaults.js

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,9 +1,11 @@
 const caseMaterials = require('./case-materials.json');
+const disclosureStatuses = require('./disclosure-statuses.json');
 
 module.exports = {
   caseSort: 'Name',
   taskSort: 'Due date',
   // Insert values here
-  caseMaterials
+  caseMaterials,
+  disclosureStatuses
 
 }
